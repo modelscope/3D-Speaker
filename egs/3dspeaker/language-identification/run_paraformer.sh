ddaafcefddf535793b302baeb469323667c70418
yfchenlucky <yfchen97@mail.ustc.edu.cn>
2024-02-29 11:03:53 +0800

    update

--- egs/3dspeaker/language-identification/run_paraformer.sh
+++ egs/3dspeaker/language-identification/run_paraformer.sh
@@ -41,20 +41,20 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
   echo "Stage4: Training the model..."
   num_gpu=$(echo $gpus | awk -F ' ' '{print NF}')
   torchrun --nproc_per_node=$num_gpu speakerlab/bin/train_para.py --config conf/eres2net_para.yaml --gpu $gpus \
-           --data $data/mejck/train/new_train_mejck_70w.csv --noise $data/musan/wav.scp --reverb $data/rirs/wav.scp --exp_dir $exp_dir
+           --data $data/3dspeaker/train/train.csv --noise $data/musan/wav.scp --reverb $data/rirs/wav.scp --exp_dir $exp_dir
 fi
 
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   # Output the prediction results.
   echo "Stage5: Predicting the test data..."
   torchrun --nproc_per_node=16 local/predict_para.py --exp_dir $exp_dir \
-           --data $data/mejck/test/wav.scp --use_gpu --gpu $gpus
+           --data $data/3dspeaker/test/wav.scp --use_gpu --gpu $gpus
 fi
 
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   # Output score metrics.
   echo "Stage6: Computing score metrics..."
   cat $exp_dir/results/predicts/predict*.txt > $exp_dir/results/predict.txt
-  python local/compute_acc.py --predict $exp_dir/results/predict.txt --ground_truth $data/mejck/test/utt2spk \
+  python local/compute_acc.py --predict $exp_dir/results/predict.txt --ground_truth $data/3dspeaker/test/utt2spk \
                   --out_dir $exp_dir/results
 fi
